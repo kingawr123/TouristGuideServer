ef753594b2f957f8ed5c071d25316406f730bc95
Kinga Wrona <kingawr123@gmail.com>
2023-12-22 16:14:21 +0100

    basic connection do mongodb cloud

---
+++ config.js
@@ -0,0 +1,1 @@
+const URL = "mongodb+srv://kingawr123:hC72bOggfKtgTtMt@touristguide.2euyivj.mongodb.net/?retryWrites=true&w=majority"
--- server.js
+++ server.js
@@ -1,8 +1,49 @@
+
+
 var express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-var app = express();
 
+var mongoose = require('mongoose');
+const { URL } = require('./config');
+
+// const url = URL
+
+const url = "mongodb+srv://kingawr123:hC72bOggfKtgTtMt@touristguide.2euyivj.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(url, 
+   { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true 
+   }
+);
+
+const tourSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true
+   },
+   description: String,
+   price: Number,
+   rating: Number
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const tour = new Tour({
+   name: 'The Sea Explorer',
+   description: 'Exploring the vast ocean blue',
+   price: 497
+});
+tour.save()
+   .then(doc => {
+      console.log(doc);
+   })
+   .catch(err => {
+      console.error(err);
+   });
+
+var app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -11,10 +52,19 @@ app.use(cors({
 }));
 
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('/', (req, res) => {
+   return res.send('Hello World');
 })
 
+app.get('/tours', (req, res) => {
+   Tour.find()
+      .then(tours => {
+         return res.json(tours);
+      })
+      .catch(err => {
+         return res.status(500).json({ msg: 'Błąd serwera' });
+      })
+})
 
 const PORT = process.env.PORT || 3000;
 
